053e3c46327ca5280051ad265c336066aee7e8f3
Alexandre Gressier <alexandre@gressier.dev>
2019-10-03 00:10:57 +0200

    Begin to take notes on "The Go Programming Language"

---
+++ books/gopl/ch1/helloworld/main/helloworld.go
@@ -0,0 +1,8 @@
+package main // Notice that we created a directory (i.e., package) "main"
+
+import "fmt" // () are optional
+
+func main() { // `func` is the keyword used to define a function
+	// Brace placement is the same as one would use in Java
+	fmt.Println("Hello, 世界") // No ending ';'
+}
