6feaddff9cc8797c1903c8a1cbfd0161a3ec15aa
Alexandre Gressier <alexandre@gressier.dev>
2019-10-04 22:52:13 +0200

    Create an IntelliJ IDEA subproject for "Rock the JVM! Scala for Beginners"

---
+++ videos/rock-the-jvm/rock-the-jvm-scala-beginners/src/playground/ScalaPlayground.scala
@@ -0,0 +1,6 @@
+package playground
+
+object ScalaPlayground extends App { // `object` instead of `class`
+  // Extending `App` automatically insert a main function
+  println("Hello, Scala!") // No ending ';'
+}
