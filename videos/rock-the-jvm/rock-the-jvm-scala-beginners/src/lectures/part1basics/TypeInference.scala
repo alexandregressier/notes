a2bfc8f0c85efab5b44fbb249777aa18448f5ca0
Alexandre Gressier <alexandre@gressier.dev>
2019-10-05 19:05:52 +0200

    Clarify comments in the "Type Inference" notes

--- videos/rock-the-jvm/rock-the-jvm-scala-beginners/src/lectures/part1basics/TypeInference.scala
+++ videos/rock-the-jvm/rock-the-jvm-scala-beginners/src/lectures/part1basics/TypeInference.scala
@@ -6,15 +6,15 @@ object TypeInference extends App {
   val x = 2 // `2` is of type `Int`, so `x` is an `Int`
   val y = x + " items" // FUNDAMENTAL: `Int` + `String` = `String`, `y` is a `String`
 
-  // The compiler is also able to infer the result type of a function by looking at its implementation:
+  // The compiler is also able to infer the result type of a function by looking at the result type of the body:
   def successor(x: Int) = x + 1 // The result type of this function is `Int`
 
-  // When explicitly writing type annotations, the compiler will still use type inference to do a type check:
+  // When explicitly writing type annotations, the compiler still does type inference to do a type check:
   //val x: Int = "Hello, world!" // ERROR: type mismatch
 
   // The compiler is not always able to figure out the result type on its own:
   def factorial(n: Int): Int = if (n == 0) 1 else n * factorial(n - 1)
   // The first branch yields an `Int`
-  // The second branch yields a product of an `Int` by a function's result type to be determined
+  // The second branch yields a product of an `Int` by the result of a function w/ a type to be determined next
   // -> But this is the current function, which has not result type defined yet!
 }
