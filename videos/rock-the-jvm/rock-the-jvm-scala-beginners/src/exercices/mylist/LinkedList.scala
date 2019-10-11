232dafa7f9572221a5efe59aa7e019ff6c7da58e
Alexandre Gressier <alexandre@gressier.dev>
2019-10-11 23:40:03 +0200

    Generify the LinkedList class using covariance

--- videos/rock-the-jvm/rock-the-jvm-scala-beginners/src/exercices/mylist/LinkedList.scala
+++ videos/rock-the-jvm/rock-the-jvm-scala-beginners/src/exercices/mylist/LinkedList.scala
@@ -5,33 +5,34 @@ import scala.annotation.tailrec
 /**
  * Singly linked list.
  */
-abstract class LinkedList {
-  def head: Int
+abstract class LinkedList[+A] {
+  def head: A
 
-  def tail: LinkedList
+  def tail: LinkedList[A]
 
-  def last: Int
+  def last: A
 
-  def init: LinkedList
+  def init: LinkedList[A]
 
   def isEmpty: Boolean
 
-  def add(x: Int): LinkedList
+  def add[B >: A](x: B): LinkedList[B]
 
-  def append(x: Int): LinkedList = appendAll(Cons(x))
+  def append[B >: A](x: B): LinkedList[B] = appendAll(Cons(x))
 
-  @tailrec
-  final def addAll(xs: LinkedList): LinkedList =
+  //@tailrec // TODO: find out why `@tailrec` is no longer working by adding a type parameter
+  final def addAll[B >: A](xs: LinkedList[B]): LinkedList[B] =
     if (xs.isEmpty) this
     else add(xs.head).addAll(xs.tail)
 
-  final def appendAll(xs: LinkedList): LinkedList = // TODO: use to use tail recursion for the `appendAll` method
+  // TODO: use to use tail recursion for the `appendAll` method
+  final def appendAll[B >: A](xs: LinkedList[B]): LinkedList[B] =
     if (isEmpty) xs
     else Cons(head, tail.appendAll(xs))
 
-  final def +(xs: LinkedList): LinkedList = appendAll(xs)
+  final def +[B >: A](xs: LinkedList[B]): LinkedList[B] = appendAll(xs)
 
-  def reverse: LinkedList
+  def reverse: LinkedList[A]
 
   protected def mkString(accumulator: String = ""): String
 
@@ -43,20 +44,20 @@ abstract class LinkedList {
  * <p>
  * Represents an empty cons cell.
  */
-object Nil extends LinkedList {
-  override def head: Int = throw new NoSuchElementException
+object Nil extends LinkedList[Nothing] {
+  override def head: Nothing = throw new NoSuchElementException
 
-  override def tail: LinkedList = throw new NoSuchElementException
+  override def tail: LinkedList[Nothing] = throw new NoSuchElementException
 
-  override def last: Int = throw new NoSuchElementException
+  override def last: Nothing = throw new NoSuchElementException
 
-  override def init: LinkedList = throw new NoSuchElementException
+  override def init: LinkedList[Nothing] = throw new NoSuchElementException
 
   override def isEmpty: Boolean = true
 
-  override def add(x: Int): LinkedList = Cons(x)
+  override def add[B >: Nothing](x: B): LinkedList[B] = Cons(x)
 
-  override def reverse: LinkedList = this
+  override def reverse: LinkedList[Nothing] = this
 
   override protected def mkString(accumulator: String): String = ""
 }
@@ -67,43 +68,44 @@ object Nil extends LinkedList {
  * @param head the value stored in the cons cell.
  * @param tail the cons cell following that value.
  */
-final class Cons(override val head: Int,
-                 override val tail: LinkedList = Nil) extends LinkedList {
+final class Cons[+A](override val head: A,
+                     override val tail: LinkedList[A] = Nil) extends LinkedList[A] {
   @tailrec
-  override def last: Int =
+  override def last: A =
     if (tail.isEmpty) head
-    else tail.asInstanceOf[Cons].last // `asInstanceOf` permits `@tailrec` usage
+    else tail.asInstanceOf[Cons[A]].last // `asInstanceOf` permits `@tailrec` usage
 
-  override def init: LinkedList = // TODO: use to use tail recursion for the `init` method
+  override def init: LinkedList[A] = // TODO: use to use tail recursion for the `init` method
     if (tail.isEmpty) Nil
-    else Cons(head, tail.asInstanceOf[Cons].init)
+    else Cons(head, tail.asInstanceOf[Cons[A]].init)
 
   override def isEmpty: Boolean = false
 
-  override def add(x: Int): LinkedList = Cons(x, this)
+  override def add[B >: A](x: B): LinkedList[B] = Cons(x, this)
 
-  override def reverse: LinkedList = Nil.addAll(this)
+  override def reverse: LinkedList[A] = Nil.addAll(this)
 
   @tailrec
   override protected def mkString(accumulator: String): String =
     if (tail.isEmpty) s"$accumulator$head"
-    else tail.asInstanceOf[Cons].mkString(s"$accumulator$head, ")
+    else tail.asInstanceOf[Cons[A]].mkString(s"$accumulator$head, ")
 }
 
 /**
  * Cons companion object.
  */
 object Cons {
-  def apply(head: Int, tail: LinkedList = Nil) = new Cons(head, tail)
+  def apply[A](head: A, tail: LinkedList[A] = Nil) = new Cons(head, tail)
+  // LEARN: in practice, the type parameter of `apply` will always be set implicitly via the arguments
 }
 
 /**
  * Singly linked list application.
  */
 object LinkedListApp extends App {
-  val empty: LinkedList = Nil
-  val singleton: LinkedList = Cons(1)
-  val couple: LinkedList = Cons(2, singleton)
+  val empty: LinkedList[Int] = Nil
+  val singleton = Cons(1)
+  val couple: LinkedList[Int] = Cons(2, singleton)
   val lost = Cons(4, Cons(8, Cons(15, Cons(16, Cons(23, Cons(42))))))
 
   println(empty)
