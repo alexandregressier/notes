a5713b6828946f7c82963e19fd6f2d91235b2e1b
Alexandre Gressier <alexandre@gressier.dev>
2019-10-11 22:11:16 +0200

    Finish to take notes about "Generics"

--- videos/rock-the-jvm/rock-the-jvm-scala-beginners/src/lectures/part2oop/Generics.scala
+++ videos/rock-the-jvm/rock-the-jvm-scala-beginners/src/lectures/part2oop/Generics.scala
@@ -49,20 +49,23 @@ object Generics extends App {
   // The variance problem: if `Cat` extends `Animal`, does a `MyList[Cat]` also extends `MyList[Animal]`?
   // 3 possible answers:
 
-  // 1. Yes: `MyList[Cat]` does extend `MyList[Animal]` -> /Covariance/
+  // 1. Yes: `MyList[Cat]` does extend `MyList[Animal]` (specific type arguments can replace the generic ones)
+  // -> /Covariance/
   class CovariantList[+A] // `+` makes the type `A` covariant
 
   // You use a `CovariantList` in the same style that you would use `Cat` for `Animal`:
   val animal: Animal = new Cat
   val covariantAnimals: CovariantList[Animal] = new CovariantList[Cat]
-  // Then, would `animals.add(new Dog)` be OK ? -> Non-trivial question
+  // Then, would `covariantAnimals.add(new Dog)` be acceptable? -> Non-trivial exercise
   // -> In theory: yes, but it would actually "pollute" the `CovariantList[Cat]` instance referenced by `animals`
 
-  // 2. No: `MyList[Cat]` and `MyList[Animal]` are distinct and cannot substitute one for another  -> /Invariance/
+  // 2. Neutral: `MyList[Cat]` and `MyList[Animal]` are distinct and cannot substitute one for another
+  // -> /Invariance/
   class InvariantList[A] // Type parameters are invariant by default
   //val invariantAnimals: InvariantList[Animal] = new InvariantList[Cat] // ERROR: type mismatch
 
-  // 3. Hell no! (Counter-intuitive concept) -> /Contravariance/
+  // 3. No (counter-intuitive concept): generic type arguments can replace the specific ones
+  // -> /Contravariance/
   // IMPORTANT: contravariance is the opposite /relation/ of covariance
   class ContravariantList[-A]  // `-` makes the type `A` contravariant
   val contravariantAnimals: ContravariantList[Cat] = new ContravariantList[Animal] // Would not compile without the `-`
@@ -74,18 +77,40 @@ object Generics extends App {
   // Bounded types
   // /Bounded types/ enable you to use your generic classes only for certain types that are either:
   // 1. A subtype of a given type (or the given type itself)
-  class Cage[A <: Animal](animal: A) // `Animal` is an /upper bounded type/
-  // The `<:` operator indicates that `A` can only be replaced by subtypes of `Animal`
+  class Cage[A <: Animal](val animal: A) // `Animal` is an /upper bounded type/
+  // The `<:` operator indicates that `A` can only be replaced by subtypes of `Animal` (or `Animal` itself)
   val cage = new Cage(new Cat)
   class Car
   //val carCage = new Cage(new Car)
-  // ERROR (not reported by IntelliJ):
+  // ERROR (unreported by IntelliJ):
   // inferred type arguments [Generics.Car] do not conform to class Cage's type parameter bounds [A <: Generics.Animal]
 
-  // IMPORTANT: just like Java, you do have to provide a type argument between `[]` if this type argument is already
-  // replacing a the type parameter of a method/class parameter
+  // IMPORTANT: just like Java, you do not have to provide a type argument between `[]` if this type argument is already
+  // replacing the type parameter of a class/method parameter
 
   // 2. A supertype of a given type (or the given type itself)
-  class Shelter[A >: Cat](cat: A) // `Cat` is a /lower bounded type/
+  class Shelter[A >: Cat](val cat: A) // `Cat` is a /lower bounded type/
+  // The `>:` operator indicates that `A` can only be replaced by supertypes of `Cat` (or `Cat` itself)
   val shelter = new Shelter(new Dog) // `Dog` shares a common supertype with `Cat`: `Animal`
+  // `new Dog` conforms to the type definition b/c it is a subtype of `Animal`, which is a supertype of `Cat`
+  // -> ESSENTIAL: but the member variable `cat` will contain an instance of Dog (it will not be casted!!!)
+
+  // IMPORTANT: bounded types solve an annoying variance problem that occurs when writing covariant collections
+  class SimpleList[+A] {
+    //def add(element: A): SimpleList[A] = new SimpleList[A]
+    // ERROR: covariant type `A` occurs in contravariant position in type `A` of value `element`
+    // -> Technical version of the non-trivial exercise: given
+    val animals: SimpleList[Animal] = new SimpleList[Cat]
+    // what happens if `animals.add(new Dog)`?
+    // -> Solution to implement: the new `SimpleList` resulting from calling `add` must have its type changed from the
+    // specific `SimpleList[Cat]` to the more generic `SimpleList[Animal]`
+    def add[B >: A](element: B): SimpleList[B] = new SimpleList[B] // ESSENTIAL
+    // We introduce a new type `B` which is a supertype of `A` and replace the type of the added in element with it as
+    // well as the result type to a list of it
+    // FUNDAMENTAL: if to a list of `A`, I add in a `B` which is a supertype of `A`, then this list will turn into a
+    // list of `B` (and not a list of `A`)
+
+    // TAKEAWAY: you want to define covariant collections, so you must use this advanced technique in order to have your
+    // method implemented correctly
+  }
 }
