39d6d94a3c543cbcc5f45790e345e1df3a0ecfe8
Alexandre Gressier <alexandre@gressier.dev>
2019-10-12 16:11:08 +0200

    Attempt to complete the exercise about predicates and transformers

--- videos/rock-the-jvm/rock-the-jvm-scala-beginners/src/exercices/mylist/LinkedList.scala
+++ videos/rock-the-jvm/rock-the-jvm-scala-beginners/src/exercices/mylist/LinkedList.scala
@@ -34,6 +34,12 @@ abstract class LinkedList[+A] {
 
   def reverse: LinkedList[A]
 
+  def map[B](t: MyTransformer[A, B]): LinkedList[B]
+
+  def flatMap[B](t: MyTransformer[A, LinkedList[B]]): LinkedList[B] // results in the concatenation of small sublists
+
+  def filter(p: MyPredicate[A]): LinkedList[A]
+
   protected def mkString(accumulator: String = ""): String
 
   override def toString: String = s"[${mkString()}]"
@@ -59,6 +65,12 @@ object Nil extends LinkedList[Nothing] {
 
   override def reverse: LinkedList[Nothing] = this
 
+  override def map[B](t: MyTransformer[Nothing, B]): LinkedList[B] = this
+
+  override def flatMap[B](t: MyTransformer[Nothing, LinkedList[B]]): LinkedList[B] = this
+
+  override def filter(p: MyPredicate[Nothing]): LinkedList[Nothing] = this
+
   override protected def mkString(accumulator: String): String = ""
 }
 
@@ -77,13 +89,26 @@ final class Cons[+A](override val head: A,
 
   override def init: LinkedList[A] = // TODO: use to use tail recursion for the `init` method
     if (tail.isEmpty) Nil
-    else Cons(head, tail.asInstanceOf[Cons[A]].init)
+    else Cons(head, tail.init)
 
   override def isEmpty: Boolean = false
 
   override def add[B >: A](x: B): LinkedList[B] = Cons(x, this)
 
   override def reverse: LinkedList[A] = Nil.addAll(this)
+
+  override def map[B](t: MyTransformer[A, B]): LinkedList[B] =
+    if (tail.isEmpty) Cons(t(head))
+    else Cons(t(head), tail.map(t))
+
+  override def flatMap[B](t: MyTransformer[A, LinkedList[B]]): LinkedList[B] =
+    if (tail.isEmpty) t(head)
+    else t(head).appendAll(tail.flatMap(t))
+
+  override def filter(p: MyPredicate[A]): LinkedList[A] =
+    if (tail.isEmpty) if (p(head)) Cons(head) else Nil
+    else if (p(head)) Cons(head, tail.filter(p))
+    else tail.filter(p)
 
   @tailrec
   override protected def mkString(accumulator: String): String =
@@ -95,8 +120,27 @@ final class Cons[+A](override val head: A,
  * Cons companion object.
  */
 object Cons {
-  def apply[A](head: A, tail: LinkedList[A] = Nil) = new Cons(head, tail)
   // LEARN: in practice, the type parameter of `apply` will always be set implicitly via the arguments
+  def apply[A](head: A, tail: LinkedList[A] = Nil) = new Cons(head, tail)
+}
+
+/**
+ * Predicate on `T`.
+ *
+ * @tparam T the type to be tested.
+ */
+trait MyPredicate[-T] { // `MyPredicate` must be contravariant in the type `T`
+  def apply(x: T): Boolean
+}
+
+/**
+ * Transformer from type `A` to type `B`.
+ *
+ * @tparam A the type to input.
+ * @tparam B the type to output.
+ */
+trait MyTransformer[-A, B] { // `MyTransformer` must be contravariant in the type `A`
+  def apply(x: A): B
 }
 
 /**
@@ -129,4 +173,28 @@ object LinkedListApp extends App {
   // Can be heterogeneous
   val fun: LinkedList[Any] = Cons("A", Cons("B", Cons("B"))) + Cons(4, Cons(5, Cons(6)))
   println(fun)
+
+  val isEvenP = new MyPredicate[Int] {
+    override def apply(x: Int): Boolean = x % 2 == 0
+  }
+
+  class StringToIntT extends MyTransformer[String, Int] {
+    override def apply(x: String): Int = x.toInt
+  }
+
+  // Higher-order functions
+  println(Cons(1, Cons(2, Cons(3)))
+    .map(new MyTransformer[Int, Int] {
+      override def apply(x: Int): Int = x * 5
+    }))
+  println(Cons(1, Cons(2, Cons(3)))
+    .flatMap(new MyTransformer[Int, LinkedList[Int]] {
+      override def apply(x: Int): LinkedList[Int] = Cons(x, Cons(x + 1))
+    }))
+  println(Cons("1", Cons("2", Cons("3", Cons("4", Cons("5", Cons("6", Cons("7", Cons("8"))))))))
+    .map(new StringToIntT)
+    .filter(isEvenP)
+    .map(new MyTransformer[Int, String] {
+      override def apply(x: Int): String = s"'$x'"
+    }))
 }
