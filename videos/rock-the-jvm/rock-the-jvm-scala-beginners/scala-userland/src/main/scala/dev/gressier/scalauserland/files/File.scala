fb67cf6762d49aa2a3753903f9e0e071876b5e5f
Alexandre Gressier <alexandre@gressier.dev>
2019-10-23 23:58:39 +0200

    Remove the `sealed` modifier of the File class

--- videos/rock-the-jvm/rock-the-jvm-scala-beginners/scala-userland/src/main/scala/dev/gressier/scalauserland/files/File.scala
+++ videos/rock-the-jvm/rock-the-jvm-scala-beginners/scala-userland/src/main/scala/dev/gressier/scalauserland/files/File.scala
@@ -4,7 +4,7 @@ import java.time.LocalDateTime
 
 /** File.
   */
-sealed class File(name: String) {
+class File(name: String) {
   val content: String = ""
   val creationDateTime: LocalDateTime = LocalDateTime.now()
   val modificationDateTime: LocalDateTime = creationDateTime
