6e5bcf9269102454118d9773b5ea2cb58f852329
Alexandre Gressier <alexandre@gressier.dev>
2019-10-23 22:15:43 +0200

    Create the Scala userland application

---
+++ videos/rock-the-jvm/rock-the-jvm-scala-beginners/scala-userland/build.sbt
@@ -0,0 +1,5 @@
+name := "scala-userland"
+
+version := "0.1.0-SNAPSHOT"
+
+scalaVersion := "2.12.10"
---
+++ videos/rock-the-jvm/rock-the-jvm-scala-beginners/scala-userland/src/main/scala/dev/gressier/scalauserland/Main.scala
@@ -0,0 +1,11 @@
+package dev.gressier.scalauserland
+
+import scala.io.StdIn
+
+/**
+ * Scala userland application.
+ */
+object Main extends App {
+  val message = StdIn.readLine()
+  println(message)
+}
