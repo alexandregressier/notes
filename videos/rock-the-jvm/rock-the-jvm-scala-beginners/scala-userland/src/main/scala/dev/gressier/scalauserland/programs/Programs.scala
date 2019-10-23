a763dd7b8835b6b5776b5682f9a8fd3954a38464
Alexandre Gressier <alexandre@gressier.dev>
2019-10-24 00:02:34 +0200

    Create the `exit` program

--- videos/rock-the-jvm/rock-the-jvm-scala-beginners/scala-userland/src/main/scala/dev/gressier/scalauserland/programs/Programs.scala
+++ videos/rock-the-jvm/rock-the-jvm-scala-beginners/scala-userland/src/main/scala/dev/gressier/scalauserland/programs/Programs.scala
@@ -7,3 +7,13 @@ import dev.gressier.scalauserland.files.File
 sealed abstract class Program[+I, +O] (name: String) extends File(name) {
   def run[I2 >: I, O2 >: O] (input: I2): O2
 }
+
+/** The `exit` program.
+  */
+case object Exit extends Program[Unit, Unit] ("exit") {
+
+  override def run[I2 >: Unit, O2 >: Unit](input: I2): O2 =
+    println("Exiting.")
+
+}
+
