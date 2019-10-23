41043d61f723ea4e6a2cfc92049f00ab109f498b
Alexandre Gressier <alexandre@gressier.dev>
2019-10-23 22:37:37 +0200

    Use the Scaladoc style with gutter asterisks aligned

--- videos/rock-the-jvm/rock-the-jvm-scala-beginners/scala-userland/src/main/scala/dev/gressier/scalauserland/Main.scala
+++ videos/rock-the-jvm/rock-the-jvm-scala-beginners/scala-userland/src/main/scala/dev/gressier/scalauserland/Main.scala
@@ -2,9 +2,8 @@ package dev.gressier.scalauserland
 
 import scala.io.StdIn
 
-/**
- * Scala userland application.
- */
+/** Scala userland application
+  */
 object Main extends App {
   val message = StdIn.readLine()
   println(message)
