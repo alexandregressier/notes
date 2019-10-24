2e3efe5be1d161939698017ce651170b51ebc8eb
Alexandre Gressier <alexandre@gressier.dev>
2019-10-24 22:22:49 +0200

    Create a new sandbox for http4s

---
+++ sandboxes/scala/http4s-sandbox/src/main/scala/dev/gressier/http4ssandbox/Main.scala
@@ -0,0 +1,9 @@
+package dev.gressier.http4ssandbox
+
+import cats.effect.{ExitCode, IO, IOApp}
+import cats.implicits._
+
+object Main extends IOApp {
+  def run(args: List[String]) =
+    SandboxServer.stream[IO].compile.drain.as(ExitCode.Success)
+}
