125ccda28679cef1ed862780360b3a53ac85125f
Alexandre Gressier <alexandre@gressier.dev>
2020-01-18 20:41:13 +0100

    Use a `StatefulWidget` for `MyApp`

--- courses/learn-flutter-dart-to-build-ios-android-apps/first_app/lib/main.dart
+++ courses/learn-flutter-dart-to-build-ios-android-apps/first_app/lib/main.dart
@@ -2,15 +2,54 @@ import 'package:flutter/material.dart';
 
 main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  var questionIndex = 0;
+
+  void answerQuestion() {
+    setState(() {
+      ++questionIndex;
+      print(questionIndex);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+    var questions = [
+      'What is your favorite color?',
+      'What is your favorite animal?',
+      'What is your favorite superhero?',
+    ];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('My first app'),
         ),
-        body: Text('This is my default text'),
+        body: Column(
+          children: <Widget>[
+            Text(questions[questionIndex]),
+            RaisedButton(
+              child: Text('Answer A'),
+              onPressed: answerQuestion,
+            ),
+            RaisedButton(
+              child: Text('Answer B'),
+              onPressed: answerQuestion,
+            ),
+            RaisedButton(
+              child: Text('Answer C'),
+              onPressed: answerQuestion,
+            ),
+            RaisedButton(
+              child: Text('Answer D'),
+              onPressed: answerQuestion,
+            ),
+          ],
+        ),
       ),
     );
   }
