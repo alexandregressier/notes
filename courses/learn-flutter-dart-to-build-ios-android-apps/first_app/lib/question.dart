3ff233f46dbe4789ee8e480b9120b70a1b1b87a2
Alexandre Gressier <alexandre@gressier.dev>
2020-01-18 22:14:52 +0100

    Style the `Question` container

--- courses/learn-flutter-dart-to-build-ios-android-apps/first_app/lib/question.dart
+++ courses/learn-flutter-dart-to-build-ios-android-apps/first_app/lib/question.dart
@@ -7,6 +7,16 @@ class Question extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Text(text);
+    return Container(
+      child: Text(
+        text,
+        style: TextStyle(
+          fontSize: 28,
+        ),
+        textAlign: TextAlign.center,
+      ),
+      width: double.infinity,
+      margin: EdgeInsets.all(10),
+    );
   }
 }
