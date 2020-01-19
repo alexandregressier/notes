fcf5144c0b3ade6c3a62b47d8ad1233f5b2076a6
Alexandre Gressier <alexandre@gressier.dev>
2020-01-19 20:06:48 +0100

    Display an ending message when the quiz is completed

--- courses/learn-flutter-dart-to-build-ios-android-apps/first_app/lib/main.dart
+++ courses/learn-flutter-dart-to-build-ios-android-apps/first_app/lib/main.dart
@@ -11,44 +11,44 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  static const questions = const [
+    {
+      'question': 'What is your favorite color?',
+      'answers': ['Red', 'Yellow', 'Green', 'Blue'],
+    },
+    {
+      'question': 'What is your favorite animal?',
+      'answers': ['Dog', 'Cat', 'Bird', 'Fish'],
+    },
+    {
+      'question': 'Who is your favorite superhero?',
+      'answers': ['Superman', 'Batman', 'Spider-man', 'Iron man'],
+    },
+  ];
   var _questionIndex = 0;
 
   void _answerQuestion() {
     setState(() {
       ++_questionIndex;
-      print(_questionIndex);
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    const questions = const [
-      {
-        'question': 'What is your favorite color?',
-        'answers': ['Red', 'Yellow', 'Green', 'Blue'],
-      },
-      {
-        'question': 'What is your favorite animal?',
-        'answers': ['Dog', 'Cat', 'Bird', 'Fish'],
-      },
-      {
-        'question': 'Who is your favorite superhero?',
-        'answers': ['Superman', 'Batman', 'Spider-man', 'Iron man'],
-      },
-    ];
-
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('My first app'),
-        ),
-        body: Column(
-          children: [
-            Question(questions[_questionIndex]['question']),
-            for (var answer in questions[_questionIndex]['answers']) Answer(answer, _answerQuestion),
-          ],
-        ),
-      ),
+          appBar: AppBar(
+            title: Text('My first app'),
+          ),
+          body: _questionIndex < questions.length
+              ? Column(children: [
+                  Question(questions[_questionIndex]['question']),
+                  for (var answer in questions[_questionIndex]['answers'])
+                    Answer(answer, _answerQuestion),
+                ])
+              : Center(
+                  child: Text('You did it!'),
+                )),
     );
   }
 }
