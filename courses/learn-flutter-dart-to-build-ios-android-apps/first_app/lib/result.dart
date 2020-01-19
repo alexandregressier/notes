c363cdfea348366d83f5991db05761400b1e0f0b
Alexandre Gressier <alexandre@gressier.dev>
2020-01-19 22:36:18 +0100

    Add a result phrase to the Result widget

--- courses/learn-flutter-dart-to-build-ios-android-apps/first_app/lib/result.dart
+++ courses/learn-flutter-dart-to-build-ios-android-apps/first_app/lib/result.dart
@@ -5,10 +5,39 @@ class Result extends StatelessWidget {
 
   Result({@required this.score});
 
+  String get resultPhrase {
+    if (score < 50) {
+      return 'You will do better next time!';
+    } else if (score < 75) {
+      return 'Not bad!';
+    } else if (score < 100) {
+      return 'You did great!';
+    } else {
+      return 'Congratulations!!';
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text('Final score: $score'),
+      child: Column(
+        children: [
+          Text(
+            'Final score: $score',
+            style: TextStyle(
+              fontSize: 36,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          Text(
+            resultPhrase,
+            style: TextStyle(
+              fontSize: 20,
+            ),
+          ),
+        ],
+        mainAxisAlignment: MainAxisAlignment.center,
+      ),
     );
   }
 }
