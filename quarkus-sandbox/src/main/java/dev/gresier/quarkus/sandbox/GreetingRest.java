f09a3cabdb270afc77d9aaa2f5229391993a6a53
Alexandre Gressier <alexandre@gressier.dev>
2020-03-20 19:16:50 +0100

    Add a the Lombok plugin to Gradle

--- quarkus-sandbox/src/main/java/dev/gresier/quarkus/sandbox/GreetingRest.java
+++ quarkus-sandbox/src/main/java/dev/gresier/quarkus/sandbox/GreetingRest.java
@@ -1,10 +1,13 @@
 package dev.gresier.quarkus.sandbox;
 
+import lombok.extern.slf4j.Slf4j;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+@Slf4j
 @Path("/hello")
 public class GreetingRest {
 
