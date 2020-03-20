84348a3a95631d35bbedc301d684ba4896797aeb
Alexandre Gressier <alexandre@gressier.dev>
2020-03-20 18:41:50 +0100

    Create a new Quarkus sandbox

---
+++ quarkus-sandbox/src/native-test/java/dev/gresier/quarkus/sandbox/NativeSimpleRestIT.java
@@ -0,0 +1,9 @@
+package dev.gresier.quarkus.sandbox;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeSimpleRestIT extends SimpleRestTest {
+
+    // Execute the same tests but in native mode.
+}
---
+++ quarkus-sandbox/src/test/java/dev/gresier/quarkus/sandbox/SimpleRestTest.java
@@ -0,0 +1,20 @@
+package dev.gresier.quarkus.sandbox;
+
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.is;
+
+@QuarkusTest
+public class SimpleRestTest {
+
+    @Test
+    public void testHelloEndpoint() {
+        given()
+                .when().get("/hello")
+                .then()
+                .statusCode(200)
+                .body(is("hello"));
+    }
+}
