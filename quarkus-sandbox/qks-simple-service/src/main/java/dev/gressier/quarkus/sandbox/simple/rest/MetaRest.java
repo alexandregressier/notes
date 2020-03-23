8eafaa4d88bf66f1d02ee7284b472d52536ad433
Alexandre Gressier <alexandre@gressier.dev>
2020-03-23 23:35:12 +0100

    Revert "Add a missing `private` visibility modifier"
    This reverts commit e028ceaa

--- quarkus-sandbox/qks-simple-service/src/main/java/dev/gressier/quarkus/sandbox/simple/rest/MetaRest.java
+++ quarkus-sandbox/qks-simple-service/src/main/java/dev/gressier/quarkus/sandbox/simple/rest/MetaRest.java
@@ -12,7 +12,7 @@ import javax.ws.rs.core.MediaType;
 public class MetaRest {
 
     @Inject
-    private MetaService metaService;
+    MetaService metaService;
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
