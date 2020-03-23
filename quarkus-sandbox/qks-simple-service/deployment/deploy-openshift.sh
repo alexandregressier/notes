235214c06c24429db157576a80e1f37a8d8faa6e
Alexandre Gressier <alexandre@gressier.dev>
2020-03-23 20:27:45 +0100

    Update the OpenShift deployment script for the simple service

--- quarkus-sandbox/qks-simple-service/deployment/deploy-openshift.sh
+++ quarkus-sandbox/qks-simple-service/deployment/deploy-openshift.sh
@@ -1,22 +1,24 @@
 #!/usr/bin/env bash
 
+readonly SERVICE_NAME='simple-service'
+
 # Build native application
-./mvnw buildNative
+./mvnw package -Pnative -Dnative-image.docker-build=true
 
 # Create a new Quarkus project
-oc new-project quarkus-sandbox
+oc new-project $SERVICE_NAME
 
-# Create a new Binary Build named "quarkus-sandbox"
-oc new-build --binary --name=quarkus-sandbox -l app=quarkus-sandbox
+# Create a new Binary Build
+oc new-build --binary --name=$SERVICE_NAME -l app=$SERVICE_NAME
 
 # Set the dockerfilePath attribute into the Build Configuration
-oc patch bc/quarkus-sandbox -p '{"spec":{"strategy":{"dockerStrategy":{"dockerfilePath":"src/main/docker/Dockerfile.native"}}}}'
+oc patch bc/$SERVICE_NAME -p '{"spec":{"strategy":{"dockerStrategy":{"dockerfilePath":"src/main/docker/Dockerfile.native"}}}}'
 
 # Start the build, uploading content from the local folder
-oc start-build quarkus-sandbox --from-dir=. --follow
+oc start-build $SERVICE_NAME --from-dir=. --follow
 
-# Create a new Application, using as Input the "quarkus-sandbox" Image Stream
-oc new-app --image-stream=quarkus-sandbox:latest
+# Create a new Application, using as input the Binary Build Image Stream
+oc new-app --image-stream=$SERVICE_NAME:latest
 
 # Expose the Service through a Route
-oc expose svc/quarkus-sandbox
+oc expose svc/$SERVICE_NAME
