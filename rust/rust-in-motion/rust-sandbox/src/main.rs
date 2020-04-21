e5b5491e0d661ad51270fedf2c73a4594a54da75
Alexandre Gressier <alexandre@gressier.dev>
2020-04-21 20:49:26 +0200

    Take notes on "Variables"

--- rust/rust-in-motion/rust-sandbox/src/main.rs
+++ rust/rust-in-motion/rust-sandbox/src/main.rs
@@ -1,3 +1,13 @@
 fn main() {
-    println!("Hello, world!");
+    let x = 5; // Every variable in Rust has a type (implicited here b/c of type inference)
+    let y: i32 = 6; // Not idiomatic: never explicit it unless there is an ambiguity
+    let z = x + y;
+    println!("z is {}", z);
+
+    //x += 1; // Variables are immutable by default (which is good for FP & concurrency)
+    let mut a = 5;
+    a += 1;
+    //++a; // Rust does not have increment & decrement operators
+
+    let flag = true;
 }
