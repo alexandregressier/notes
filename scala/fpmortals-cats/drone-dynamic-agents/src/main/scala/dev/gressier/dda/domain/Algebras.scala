400cf70188db34d7b93752bc529d6bfd35d53571
Alexandre Gressier <alexandre@gressier.dev>
2020-05-08 23:57:58 +0200

    Define the algebras of the project

---
+++ scala/fpmortals-cats/drone-dynamic-agents/src/main/scala/dev/gressier/dda/domain/Algebras.scala
@@ -0,0 +1,25 @@
+package dev.gressier.dda.domain
+
+import cats.data.NonEmptyList
+import dev.gressier.dda.time.Epoch
+
+/** Drone.
+  */
+trait Drone[F[_]] {
+  def getBacklog: F[Int]
+  def getAgents : F[Int]
+}
+
+/** Machines Node.
+ */
+final case class MachineNode(id: String)
+
+/** Machines.
+ */
+trait Machines[F[_]] {
+  def getTime                  : F[Epoch]
+  def getManaged               : F[NonEmptyList[MachineNode]]
+  def getAlive                 : F[Map[MachineNode, Epoch]]
+  def start(node: MachineNode): F[MachineNode]
+  def stop (node: MachineNode): F[MachineNode]
+}
